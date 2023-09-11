a1af20637f680d95cb955aa6885454ad2bec5cfe
Guillem <you@example.com>
2023-09-11 10:55:29 +0200

    Improve error handling and fix bug

--- renderer.go
+++ renderer.go
@@ -11,7 +11,11 @@ import (
 )
 
 type jetRenderer struct {
-	set *jet.Set
+	Set *jet.Set
+}
+
+func New(loader jet.Loader, opts ...jet.Option) *jetRenderer {
+	return &jetRenderer{jet.NewSet(loader, opts...)}
 }
 
 var _ render.HTMLRender = &jetRenderer{}
@@ -32,28 +36,40 @@ type renderInstance struct {
 
 var _ render.Render = &renderInstance{}
 
-func (r *renderInstance) Render(w http.ResponseWriter) error {
+func (r *renderInstance) render(w http.ResponseWriter) error {
 	if strings.ContainsRune(r.name, '#') {
 		splitted := strings.Split(r.name, "#")
 		if len(splitted) != 2 {
-			return errors.New("only one # is allowed in the template name as a template fragment")
+			return errors.New("only one '#' is allowed in the template name as a template fragment")
 		}
 
-		expr := fmt.Sprintf(`{{ import "%s" }} {{ block %s() . }} {{ end }}`, splitted[0], splitted[1])
-		t, err := r.set.Parse(r.name, expr)
+		templatePath := splitted[0]
+		blockName := splitted[1]
+
+		expr := fmt.Sprintf(`{{ import "%s" }} {{ yield %s() . }}`, templatePath, blockName)
+		t, err := r.Set.Parse(templatePath, expr)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to parse expr '%s': %w", expr, err)
 		}
 
 		return t.Execute(w, nil, r.data)
 	}
 
-	t, err := r.set.GetTemplate(r.name)
+	t, err := r.Set.GetTemplate(r.name)
 	if err != nil {
 		return err
 	}
 
 	return t.Execute(w, nil, r.data)
+}
+
+func (r *renderInstance) Render(w http.ResponseWriter) error {
+	if err := r.render(w); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
+	}
+	w.WriteHeader(http.StatusOK)
+	return nil
 }
 
 func (*renderInstance) WriteContentType(w http.ResponseWriter) {
