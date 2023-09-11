89e4bdc4e71a8603406c513ad35cdf4962d184c3
Guillem <you@example.com>
2023-09-11 10:01:29 +0200

    Fill methods

--- renderer.go
+++ renderer.go
@@ -1,30 +1,61 @@
 package jetr
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gin-gonic/gin/render"
 )
 
 type jetRenderer struct {
-	*jet.Set
+	set *jet.Set
 }
 
 var _ render.HTMLRender = &jetRenderer{}
 
-func (r *jetRenderer) Instance(name string, data interface{}) render.Render {
-	return &jetRender{}
+func (r *jetRenderer) Instance(name string, data any) render.Render {
+	return &renderInstance{
+		jetRenderer: r,
+		name:        name,
+		data:        data,
+	}
 }
 
-type jetRender struct{}
-
-var _ render.Render = &jetRender{}
-
-func (r *jetRender) Render(wr http.ResponseWriter) error {
-	panic("unimplemented")
+type renderInstance struct {
+	*jetRenderer
+	name string
+	data any
 }
 
-func (*jetRender) WriteContentType(w http.ResponseWriter) {
-	panic("unimplemented")
+var _ render.Render = &renderInstance{}
+
+func (r *renderInstance) Render(w http.ResponseWriter) error {
+	if strings.ContainsRune(r.name, '#') {
+		splitted := strings.Split(r.name, "#")
+		if len(splitted) != 2 {
+			return errors.New("only one # is allowed in the template name as a template fragment")
+		}
+
+		expr := fmt.Sprintf(`{{ import "%s" }} {{ block %s() . }} {{ end }}`, splitted[0], splitted[1])
+		t, err := r.set.Parse(r.name, expr)
+		if err != nil {
+			return err
+		}
+
+		return t.Execute(w, nil, r.data)
+	}
+
+	t, err := r.set.GetTemplate(r.name)
+	if err != nil {
+		return err
+	}
+
+	return t.Execute(w, nil, r.data)
+}
+
+func (*renderInstance) WriteContentType(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 }
